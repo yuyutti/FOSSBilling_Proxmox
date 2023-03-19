06afb49a04ef6addaf3cae01ea33f351700ebd5e
Belle Aerni <belleaerni@gmail.com>
2023-03-19 15:39:15 -0700

    Order management improvement

--- Api/Admin.php
+++ Api/Admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Proxmox module for FOSSBilling
  *
@@ -18,7 +19,7 @@ namespace Box\Mod\Serviceproxmox\Api;
  * Hosting service management 
  */
 class Admin extends \Api_Abstract
-{  
+{
     /**
      * Get list of servers
      * 
@@ -26,50 +27,50 @@ class Admin extends \Api_Abstract
      */
     public function server_get_list($data)
     {
-		$servers = $this->di['db']->find('service_proxmox_server');
-		$servers_grouped = array();
+        $servers = $this->di['db']->find('service_proxmox_server');
+        $servers_grouped = array();
         foreach ($servers as $server) {
-			$servers_grouped[$server['group']]['group'] = $server->group;
-			$servers_grouped[$server['group']]['servers'][$server['id']] = array(
-				'id'			=> $server->id,
-				'name' 			=> $server->name,
-				'group' 		=> $server->group,
-				'ipv4' 			=> $server->ipv4,
-				'ipv6' 			=> $server->ipv6,
-				'hostname' 		=> $server->hostname,
-				'access' 		=> $this->getService()->find_access($server),
-				'used_slots'	=> $server->slots - $this->used_slots($server->id),
-				'slots' 		=> $server->slots,
-				'active'		=> $server->active,
-			);
+            $servers_grouped[$server['group']]['group'] = $server->group;
+            $servers_grouped[$server['group']]['servers'][$server['id']] = array(
+                'id'            => $server->id,
+                'name'          => $server->name,
+                'group'         => $server->group,
+                'ipv4'          => $server->ipv4,
+                'ipv6'          => $server->ipv6,
+                'hostname'      => $server->hostname,
+                'access'        => $this->getService()->find_access($server),
+                'used_slots'    => $server->slots - $this->used_slots($server->id),
+                'slots'         => $server->slots,
+                'active'        => $server->active,
+            );
         }
         return $servers_grouped;
     }
 
-	/* Get server details from order id */
-	public function server_get_from_order($data)
+    /* Get server details from order id */
+    public function server_get_from_order($data)
     {
         $required = array(
             'order_id'    => 'Order id is missing',
         );
+
         $this->di['validator']->checkRequiredParamsForArray($required, $data);
 
-		// Retrieve order
-		if(!isset($data['order_id'])) {
-            throw new \Exception('Order id is required');
+        $service = $this->di['db']->findOne(
+            'service_proxmox',
+            "order_id=:id",
+            array(':id' => $data['order_id'])
+        );
+
+        if (!$service) {
+            return null;
         }
-        $service = $this->di['db']->findOne('service_proxmox',
-                "order_id=:id", 
-                array(':id'=>$data['order_id']));
-        if(!$service) {
-			$data = array ('server_id' => $service['server_id']);
-			return null;
-        }
-		$data = array ('server_id' => $service['server_id']);
-		$output = $this->server_get($data);
+
+        $data = array('server_id' => $service['server_id']);
+        $output = $this->server_get($data);
         return $output;
     }
-	
+
     /**
      * Get server details
      * 
@@ -80,77 +81,78 @@ class Admin extends \Api_Abstract
      */
     public function server_get($data)
     {
-		// Retrieve associated server
-		$server  = $this->di['db']->findOne('service_proxmox_server','id=:id',array(':id'=>$data['server_id']));
+        // Retrieve associated server
+        $server  = $this->di['db']->findOne('service_proxmox_server', 'id=:id', array(':id' => $data['server_id']));
 
-		//TODO: Update settings
-		$output = array(
-			'id'			=> $server->id,
-			'name' 			=> $server->name,
-			'group' 		=> $server->group,
-			'ipv4' 			=> $server->ipv4,
-			'ipv6' 			=> $server->ipv6,
-			'hostname' 		=> $server->hostname,
+        //TODO: Update settings
+        $output = array(
+            'id'            => $server->id,
+            'name'          => $server->name,
+            'group'         => $server->group,
+            'ipv4'          => $server->ipv4,
+            'ipv6'          => $server->ipv6,
+            'hostname'      => $server->hostname,
             'realm'         => $server->realm,
-			//'access' 		=> $this->getService()->find_access($server),
-			//'used_slots'	=> $server->slots - $this->used_slots($server->id),
-			'slots' 		=> $server->slots,
-			'root_user' 	=> $server->root_user,
-			'root_password' => $server->root_password,
-			'admin_password'=> $server->admin_password,
-			'active'		=> $server->active,
-		);
+            //'access' 		=> $this->getService()->find_access($server),
+            //'used_slots'	=> $server->slots - $this->used_slots($server->id),
+            'slots'         => $server->slots,
+            'root_user'     => $server->root_user,
+            'root_password' => $server->root_password,
+            'admin_password'=> $server->admin_password,
+            'active'        => $server->active,
+        );
         return $output;
     }
-    
+
     /*
 		Update product informations
 	*/
     public function product_update($data)
     {
         $required = array(
-			'id'			=> 'Product id is missing',
-            'group'    		=> 'Server group is missing',
-            'filling'   	=> 'Filling method is missing',
-			'show_stock'    => 'Stock display is missing',
-			'virt'			=> 'Virtualization type is missing',
-			'storage'		=> 'Target storage is missing',
-			'memory'		=> 'Memory is missing',
-			'cpu'			=> 'CPU cores is missing',
-			'network'		=> 'Network net0 is missing',
-			'ide0'			=> 'Storage ide0 is missing',
-			'clone'			=> 'Clone info is missing'
+            'id'            => 'Product id is missing',
+            'group'         => 'Server group is missing',
+            'filling'       => 'Filling method is missing',
+            'show_stock'    => 'Stock display is missing',
+            'virt'          => 'Virtualization type is missing',
+            'storage'       => 'Target storage is missing',
+            'memory'        => 'Memory is missing',
+            'cpu'           => 'CPU cores is missing',
+            'network'       => 'Network net0 is missing',
+            'ide0'          => 'Storage ide0 is missing',
+            'clone'         => 'Clone info is missing'
         );
+
         $this->di['validator']->checkRequiredParamsForArray($required, $data);
 
-		// Retrieve associated product
-		$product  = $this->di['db']->findOne('product','id=:id',array(':id'=>$data['id']));
-		
-		$config = array(
-			'group'			=> $data['group'],
-			'filling'		=> $data['filling'],
-			'show_stock'	=> $data['show_stock'],
-			'virt'			=> $data['virt'],
-			'storage'		=> $data['storage'],
-			'ostemplate'	=> $data['ostemplate'],
-			'cdrom'			=> $data['cdrom'],
-			'memory'		=> $data['memory'],
-			'cpu'			=> $data['cpu'],
-			'network'		=> $data['network'],
-			'ide0'			=> $data['ide0'],
-			'clone'			=> $data['clone'],
-			'cloneid'		=> $data['cloneid']
-		);
-		
-		$product->config     	= json_encode($config);
+        // Retrieve associated product
+        $product  = $this->di['db']->findOne('product', 'id=:id', array(':id' => $data['id']));
+
+        $config = array(
+            'group'         => $data['group'],
+            'filling'       => $data['filling'],
+            'show_stock'    => $data['show_stock'],
+            'virt'          => $data['virt'],
+            'storage'       => $data['storage'],
+            'ostemplate'    => $data['ostemplate'],
+            'cdrom'         => $data['cdrom'],
+            'memory'        => $data['memory'],
+            'cpu'           => $data['cpu'],
+            'network'       => $data['network'],
+            'ide0'          => $data['ide0'],
+            'clone'         => $data['clone'],
+            'cloneid'       => $data['cloneid']
+        );
+
+        $product->config         = json_encode($config);
         $product->updated_at    = date('Y-m-d H:i:s');
         $this->di['db']->store($product);
-		
-		$this->di['logger']->info('Update Proxmox product %s', $product->id);
+
+        $this->di['logger']->info('Update Proxmox product %s', $product->id);
         return true;
     }
-	
-	/**
+
+    /**
      * Create new hosting server 
      * 
      * @param string $name - server name
@@ -166,35 +168,35 @@ class Admin extends \Api_Abstract
     public function server_create($data)
     {
         $required = array(
-            'name'    => 'Server name is missing',
-            'slots'      => 'Slots are missing',
-			'root_user'      => 'Root user is missing',
-			'root_password'      => 'Root password is missing',
-			'realm'      => 'Proxmox user realm is missing',
+            'name'              => 'Server name is missing',
+            'slots'             => 'Slots are missing',
+            'root_user'         => 'Root user is missing',
+            'root_password'     => 'Root password is missing',
+            'realm'             => 'Proxmox user realm is missing',
         );
         $this->di['validator']->checkRequiredParamsForArray($required, $data);
 
-		$server 				= $this->di['db']->dispense('service_proxmox_server');
-        $server->name     		= $data['name'];
-		$server->group     		= $data['group'];
-		$server->ipv4     		= $data['ipv4'];
-		$server->ipv6     		= $data['ipv6'];
-		$server->hostname     	= $data['hostname'];
-		$server->realm     		= $data['realm'];
-		$server->slots     		= $data['slots'];
-		$server->root_user     	= $data['root_user'];
-		$server->root_password	= $data['root_password'];
-		$server->config     	= $data['config'];
-		$server->active     	= $data['active'];
-        $server->created_at    	= date('Y-m-d H:i:s');
-        $server->updated_at    	= date('Y-m-d H:i:s');
+        $server                     = $this->di['db']->dispense('service_proxmox_server');
+        $server->name               = $data['name'];
+        $server->group              = $data['group'];
+        $server->ipv4               = $data['ipv4'];
+        $server->ipv6               = $data['ipv6'];
+        $server->hostname           = $data['hostname'];
+        $server->realm              = $data['realm'];
+        $server->slots              = $data['slots'];
+        $server->root_user          = $data['root_user'];
+        $server->root_password      = $data['root_password'];
+        $server->config             = $data['config'];
+        $server->active             = $data['active'];
+        $server->created_at         = date('Y-m-d H:i:s');
+        $server->updated_at         = date('Y-m-d H:i:s');
         $this->di['db']->store($server);
-		
-		$this->di['logger']->info('Create Proxmox server %s', $server->id);
-		
-		return true;
+
+        $this->di['logger']->info('Create Proxmox server %s', $server->id);
+
+        return true;
     }
-	
+
     /**
      * Delete server
      * 
@@ -209,7 +211,7 @@ class Admin extends \Api_Abstract
         );
         $this->di['validator']->checkRequiredParamsForArray($required, $data);
         $server = $this->di['db']->getExistingModelById('service_proxmox_server', $data['id'], 'Server not found');
-		$this->di['db']->trash($server);
+        $this->di['db']->trash($server);
     }
 
     /**
@@ -227,36 +229,36 @@ class Admin extends \Api_Abstract
      */
     public function server_update($data)
     {
-		$required = array(
+        $required = array(
             'name'    => 'Server name is missing',
             'slots'      => 'Slots are missing',
-			'root_user'      => 'Root user is missing',
-			'root_password'      => 'Root password is missing',
-			'realm'      => 'Proxmox user realm is missing',
+            'root_user'      => 'Root user is missing',
+            'root_password'      => 'Root password is missing',
+            'realm'      => 'Proxmox user realm is missing',
         );
         $this->di['validator']->checkRequiredParamsForArray($required, $data);
 
-		// Retrieve associated server
-		$server  = $this->di['db']->findOne('service_proxmox_server','id=:id',array(':id'=>$data['server_id']));
-		
-        $server->name     		= $data['name'];
-		$server->group     		= $data['group'];
-		$server->ipv4     		= $data['ipv4'];
-		$server->ipv6     		= $data['ipv6'];
-		$server->hostname     	= $data['hostname'];
-		$server->realm     		= $data['realm'];
-		$server->slots     		= $data['slots'];
-		$server->root_user     	= $data['root_user'];
-		$server->root_password	= $data['root_password'];
-		$server->config     	= $data['config'];
-		$server->active     	= $data['active'];
-        $server->created_at    	= date('Y-m-d H:i:s');
-        $server->updated_at    	= date('Y-m-d H:i:s');
+        // Retrieve associated server
+        $server  = $this->di['db']->findOne('service_proxmox_server', 'id=:id', array(':id' => $data['server_id']));
+
+        $server->name             = $data['name'];
+        $server->group             = $data['group'];
+        $server->ipv4             = $data['ipv4'];
+        $server->ipv6             = $data['ipv6'];
+        $server->hostname         = $data['hostname'];
+        $server->realm             = $data['realm'];
+        $server->slots             = $data['slots'];
+        $server->root_user         = $data['root_user'];
+        $server->root_password    = $data['root_password'];
+        $server->config         = $data['config'];
+        $server->active         = $data['active'];
+        $server->created_at        = date('Y-m-d H:i:s');
+        $server->updated_at        = date('Y-m-d H:i:s');
         $this->di['db']->store($server);
-		
-		$this->di['logger']->info('Update Proxmox server %s', $server->id);
+
+        $this->di['logger']->info('Update Proxmox server %s', $server->id);
         return true;
-	
+
         /*$required = array(
             'server_id'    => 'Server id is missing',
         );
@@ -288,32 +290,34 @@ class Admin extends \Api_Abstract
         $server = $this->di['db']->getExistingModelById('service_proxmox_server', $data['id'], 'Server not found');
         return (bool) $this->getService()->test_connection($server);
     }
-	
-	/*
+
+    /*
 		Get list of server groups
      */
     public function server_groups()
     {
         $server = $this->di['db']->dispense('service_proxmox_server');
-		$sql = "SELECT DISTINCT `group` FROM `service_proxmox_server` WHERE `active` = 1";
+        $sql = "SELECT DISTINCT `group` FROM `service_proxmox_server` WHERE `active` = 1";
         $groups = $this->di['db']->getAll($sql);
-		return $groups;
+        return $groups;
     }
-	
-	/*
+
+    /*
 		Retrieve empty slots for each server
 	*/
-	public function used_slots($server_id)
-	{
-		$sql = "SELECT `service_proxmox`.server_id, COUNT(*) AS used
+    public function used_slots($server_id)
+    {
+        $sql = "SELECT `service_proxmox`.server_id, COUNT(*) AS used
 				FROM `client_order` 
 				INNER JOIN `service_proxmox` ON `client_order`.service_id=`service_proxmox`.id 
-				WHERE `client_order`.service_type = 'proxmox' AND `client_order`.status = 'active' AND `service_proxmox`.server_id = ".$server_id."
+				WHERE `client_order`.service_type = 'proxmox' AND `client_order`.status = 'active' AND `service_proxmox`.server_id = " . $server_id . "
 				GROUP BY `service_proxmox`.server_id";
         $active_orders = $this->di['db']->getAll($sql);
-		if(empty($active_orders[0]['used'])) {$active_orders[0]['used']=0;}
-		return $active_orders[0]['used'];
-	}
+        if (empty($active_orders[0]['used'])) {
+            $active_orders[0]['used'] = 0;
+        }
+        return $active_orders[0]['used'];
+    }
 
     public function _getService($data)
     {
