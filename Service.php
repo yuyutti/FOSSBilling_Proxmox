233dd4cce7f0570f8ad8891239c18e164eca94dd
Christoph Schläpfer <c.schlaepfer@datact.ch>
2023-07-11 16:21:22 +0200

    Small fix for reinstallation

--- Service.php
+++ Service.php
@@ -87,7 +87,14 @@ class Service implements \FOSSBilling\InjectionAwareInterface
 		$filesystem = new Filesystem();
 		// list content of pmxconfig folder using symfony finder
 		$finder = new Finder();
+
+		// check if pmxconfig folder exists
+		if (!$filesystem->exists(PATH_ROOT . '/pmxconfig')) {
+			$filesystem->mkdir(PATH_ROOT . '/pmxconfig');
+		}
+
 		$pmxbackup_dir = $finder->in(PATH_ROOT . '/pmxconfig')->files()->name('proxmox_uninstall_*.sql');
+
 		// find newest file in pmxbackup_dir according to timestamp
 		$pmxbackup_file = array();
 		foreach ($pmxbackup_dir as $file) {
@@ -116,132 +123,24 @@ class Service implements \FOSSBilling\InjectionAwareInterface
 				} else {
 					throw new \Box_Exception("The version number of the sql dump is bigger than the current version number of the module. Please check the installed Module version.", null, 9684);
 				}
-				$this->di['db']->exec($dump);
 			}
 		} else 
 		{
+		// Run migrations from smallest to biggest version number
+		$migrations = glob(__DIR__ . '/migrations/*.sql');
+		// Sort migrations by version number (Filenames: 0.0.1.sql, 0.0.2.sql etc.)
+		usort($migrations, function ($a, $b) {
+			return version_compare(basename($a, '.sql'), basename($b, '.sql'));
+		});
 		
+		foreach ($migrations as $migration) {
+			// get version from filename
+			$filename = basename($migration, '.sql');
+			$version = str_replace('_', '.', $filename);
+			// run migration
+			$this->di['db']->exec(file_get_contents($migration));
+			}
 		
-		// execute sql script if needed
-
-		$sql = "
-        CREATE TABLE IF NOT EXISTS `service_proxmox_server` (
-            `id` bigint(20) NOT NULL AUTO_INCREMENT,
-            `name` varchar(255) DEFAULT NULL,
-            `group` varchar(255) DEFAULT NULL,
-            `ipv4` varchar(255) DEFAULT NULL,
-            `ipv6` varchar(255) DEFAULT NULL,
-            `hostname` varchar(255) DEFAULT NULL,
-			`mac` varchar(255) DEFAULT NULL,
-			`realm` varchar(255) DEFAULT NULL,
-			`cpu_cores` bigint(20) DEFAULT NULL,
-			`cpu_cores_allocated` bigint(20) DEFAULT NULL,
-			`ram` bigint(20) DEFAULT NULL,
-			`ram_allocated` bigint(20) DEFAULT NULL,
-			`root_user` varchar(255) DEFAULT NULL,
-            `root_password` varchar(255) DEFAULT NULL,
-			`tokenname` varchar(255) DEFAULT NULL,
-			`tokenvalue` varchar(255) DEFAULT NULL,			
-            `config` text,
-			`active` bigint(20) DEFAULT NULL,
-            `created_at` varchar(35) DEFAULT NULL,
-            `updated_at` varchar(35) DEFAULT NULL,
-            PRIMARY KEY (`id`)
-            ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-		$this->di['db']->exec($sql);
-
-		$sql = "
-        CREATE TABLE IF NOT EXISTS `service_proxmox` (
-            `id` bigint(20) NOT NULL AUTO_INCREMENT,
-			`client_id` bigint(20) DEFAULT NULL,
-			`order_id` bigint(20) DEFAULT NULL,
-			`server_id` bigint(20) DEFAULT NULL,
-			`vmid` bigint(20) DEFAULT NULL,
-			`ipv4` varchar(255) DEFAULT NULL,
-            `ipv6` varchar(255) DEFAULT NULL,
-			`hostname` varchar(255) DEFAULT NULL,
-			`password` varchar(255) DEFAULT NULL,
-            `config` text,
-			`status` varchar(255) DEFAULT NULL,	
-			`storage` varchar(255) DEFAULT NULL,
-			`cpu_cores` varchar(255) DEFAULT NULL,
-            `created_at` varchar(35) DEFAULT NULL,
-            `updated_at` varchar(35) DEFAULT NULL,
-            PRIMARY KEY (`id`),
-			KEY `client_id_idx` (`client_id`)
-            ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-		$this->di['db']->exec($sql);
-
-		$sql = "
-        CREATE TABLE IF NOT EXISTS `service_proxmox_users` (
-            `id` bigint(20) NOT NULL AUTO_INCREMENT,
-			`client_id` bigint(20) DEFAULT NULL,
-			`server_id` bigint(20) DEFAULT NULL,
-			`admin_tokenname` varchar(255) DEFAULT NULL,
-			`admin_tokenvalue` varchar(255) DEFAULT NULL,
-			`view_tokenname` varchar(255) DEFAULT NULL,
-			`view_tokenvalue` varchar(255) DEFAULT NULL,
-            `created_at` varchar(35) DEFAULT NULL,
-            `updated_at` varchar(35) DEFAULT NULL,
-            PRIMARY KEY (`id`),
-			KEY `client_id_idx` (`client_id`),
-			KEY `server_id_idx` (`server_id`)
-            ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-		$this->di['db']->exec($sql);
-
-		// create table for storage class information
-		$sql = "
-		CREATE TABLE IF NOT EXISTS `service_proxmox_storageclass` (
-			`id` bigint(20) NOT NULL AUTO_INCREMENT,
-			`storageclass` varchar(35) DEFAULT NULL,
-			PRIMARY KEY (`id`)
-			) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
-		$this->di['db']->exec($sql);
-
-
-		// create table for server storage
-		// server_id and storage form a unique key
-		$sql = "
-		CREATE TABLE IF NOT EXISTS `service_proxmox_storage` (
-			`id` bigint(20) NOT NULL AUTO_INCREMENT,
-			`server_id` bigint(20) DEFAULT NULL,
-			`storage` varchar(255) DEFAULT NULL,
-			`type` varchar(255) DEFAULT NULL,
-			`content` varchar(255) DEFAULT NULL,
-			`active` bigint(20) DEFAULT NULL,
-			`storageclass` varchar(35) DEFAULT NULL,
-			`size` bigint(20) DEFAULT NULL,
-			`used` bigint(20) DEFAULT NULL,
-			`free` bigint(20) DEFAULT NULL,
-			PRIMARY KEY (`id`),
-			UNIQUE KEY `server_storage_unique` (`server_id`, `storage`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
-		$this->di['db']->exec($sql);
-
-		// Create table for lxc appliance templates
-		$sql = "
-		CREATE TABLE IF NOT EXISTS `service_proxmox_templates` (
-			`id` bigint(20) NOT NULL AUTO_INCREMENT,
-			`source` varchar(255) DEFAULT NULL,
-			`type` varchar(255) DEFAULT NULL,
-			`package` varchar(255) DEFAULT NULL,
-			`section` varchar(255) DEFAULT NULL,
-			`maintainer` varchar(255) DEFAULT NULL,
-			`location` varchar(255) DEFAULT NULL,
-			`headline` varchar(255) DEFAULT NULL,
-			`os` varchar(255) DEFAULT NULL,
-			`template` varchar(255) DEFAULT NULL,
-			`description` varchar(255) DEFAULT NULL,
-			`architecture` varchar(255) DEFAULT NULL,
-			`md5sum` varchar(255) DEFAULT NULL,
-			`sha512sum` varchar(255) DEFAULT NULL,
-			`version` varchar(255) DEFAULT NULL,
-			`infopage` varchar(255) DEFAULT NULL,
-			
-			PRIMARY KEY (`id`)
-		) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
-		$this->di['db']->exec($sql);
-
 		// Create table for vm 
 		}
 		return true;
